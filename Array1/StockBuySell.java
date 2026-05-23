8ee2b020b17a33f772f9cd3df67baefdf786853a
Prince Kahar <kaharprince2006@gmail.com>
2026-05-23 21:56:46 +0530

    new

---
+++ Array1/StockBuySell.java
@@ -0,0 +1,22 @@
+package Array1;
+
+public class StockBuySell {
+    public static int maxProfit(int[] prices) {
+        int BP = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (BP < prices[i]) {
+                int profit = prices[i] - BP;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                BP = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = new int[] { 7, 1, 5, 4, 6, 3 };
+        System.out.println(maxProfit(prices));
+    }
+}
