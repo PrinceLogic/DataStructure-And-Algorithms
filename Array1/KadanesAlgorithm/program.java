cbc97a3db77d5bfcd07e9891d56df4e620b3aef8
Prince Kahar <kaharprince2006@gmail.com>
2026-05-27 21:31:16 +0530

    Kadanes Algo

---
+++ Array1/KadanesAlgorithm/program.java
@@ -0,0 +1,23 @@
+
+class program {
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = 0;
+
+        for (int num : nums) {
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+            currentSum += num;
+
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        program p1 = new program();
+        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        System.out.println(p1.maxSubArray(nums));
+    }
+}
