736b1a54c5f32397fa87e87872877d36c7557b26
Prince Kahar <kaharprince2006@gmail.com>
2026-05-31 22:29:32 +0530

    new

---
+++ String/LongestCommonPrefix.java
@@ -0,0 +1,34 @@
+package String;
+
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        // If the array is empty, there is no common prefix
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        // Start by assuming the first string is the common prefix
+        String prefix = strs[0];
+
+        // Loop through the remaining strings in the array
+        for (int i = 1; i < strs.length; i++) {
+            // While the current string does not start with the prefix
+            while (strs[i].indexOf(prefix) != 0) {
+                // Shorten the prefix by 1 character from the end
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                // If the prefix becomes empty, there is no common prefix at all
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = { "flower", "flow", "flight" };
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
