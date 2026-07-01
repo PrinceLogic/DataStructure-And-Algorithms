3e0192059950c3883852debe13a94155e082d5d0
Prince Kahar <kaharprince2006@gmail.com>
2026-07-01 22:25:20 +0530

    new

---
+++ Array1/searchOnMatrix.java
@@ -0,0 +1,41 @@
+class searchOnMatrix {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        // Treat the matrix as a 1D array
+        int left = 0;
+        int right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Map the 1D index back to 2D matrix coordinates
+            int midValue = matrix[mid / n][mid % n];
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 3, 5, 7 },
+                { 10, 11, 16, 20 },
+                { 23, 30, 34, 60 }
+        };
+        int target = 3;
+        System.out.println(searchMatrix(matrix, target));
+    }
+}
