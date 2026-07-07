85b07bcdd1f046b85d4d2462f37d5071568b1346
Prince Kahar <kaharprince2006@gmail.com>
2026-07-07 21:53:23 +0530

    new

--- Array1/pivotIndex.java
+++ Array1/pivotIndex.java
@@ -3,23 +3,16 @@ class pivotIndex {
         int totalSum = 0;
         int leftSum = 0;
 
-        // Step 1: Calculate the total sum of the array
         for (int num : nums) {
             totalSum += num;
         }
 
-        // Step 2: Iterate to find the pivot index
         for (int i = 0; i < nums.length; i++) {
-            // rightSum = totalSum - leftSum - nums[i]
             if (leftSum == totalSum - leftSum - nums[i]) {
                 return i;
             }
-
-            // Add current element to leftSum for the next iteration
             leftSum += nums[i];
         }
-
-        // Step 3: No pivot index found
         return -1;
     }
 
